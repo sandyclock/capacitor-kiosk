e627495a83a4ab10343c90608ba12d226c80820c
Li Tan <tanli.stonybrook@gmail.com>
2023-09-05 12:16:10 -0700

    work on launcher selection

---
+++ android/FakeLauncherActivity.java
@@ -0,0 +1,11 @@
+package jk.cordova.plugin.kiosk;
+
+import android.app.Activity;
+
+import com.getcapacitor.BridgeActivity;
+
+/*
+ * https://stackoverflow.com/questions/27991656/how-to-set-default-app-launcher-programmatically
+ */
+public class FakeLauncherActivity extends Activity {
+}
